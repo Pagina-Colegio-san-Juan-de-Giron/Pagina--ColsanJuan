5aa8778a87c80f63e92f67ea82a70f8def199011
Página Colegio san Juan de Giron <144557927+Pagina-Colegio-san-Juan-de-Giron@users.noreply.github.com>
2023-09-13 20:03:29 +0000

    base nombre colegio (no responsive)

--- pag-sanjuan/app/Componentes/Header.tsx
+++ pag-sanjuan/app/Componentes/Header.tsx
@@ -1,6 +1,12 @@
-import "./Header.css"
-import Escudo from "./Imágenes/escudo_auth.jpg"
+import "./Header.css";
+import Image from 'next/image';
+import escudo from "./Imágenes/escudo_auth.jpg";
+import { Alice } from 'next/font/google'
 
+const alice = Alice({ 
+  subsets: ['latin-ext'],
+  weight: ['400']
+})
 
 export default function Header() {
     return (
@@ -8,9 +14,17 @@ export default function Header() {
 
             <div className="container-header">
 
-                <image className="Escudo" src={Escudo} alt="Escudo" />
+                <Image className="Escudo" src={escudo} alt="Escudo"/>
 
-                    <div className="NombreColegio">
+                    <div className="NombreColegio" >
+
+                      <div className="contenedor-letras">
+                        <div className="contenedor-span">
+                          <span className="Texto"><span className={alice.className}>Colegio</span></span>
+                          <span className="TextoMedio"><span className={alice.className}>SAN JUAN</span></span>
+                          <span className="Texto"><span className={alice.className}>De Girón</span></span>
+                        </div>
+                      </div>
 
                     </div>
 
