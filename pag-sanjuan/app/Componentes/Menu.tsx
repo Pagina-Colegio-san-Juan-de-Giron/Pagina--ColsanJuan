6b575a823d29307fef44872b00c1acb58fde15c2
Página Colegio san Juan de Giron <144557927+Pagina-Colegio-san-Juan-de-Giron@users.noreply.github.com>
2023-09-27 18:47:08 +0000

    mejoras submenu celular

--- pag-sanjuan/app/Componentes/Menu.tsx
+++ pag-sanjuan/app/Componentes/Menu.tsx
@@ -8,22 +8,40 @@ import BotonCerrar from "./BotonCerrar";
 
 export default function Menu() {
   const [cerrado, setcerrado] = useState(true)
-  
+  let celular = false
+
+  const definirancho = () =>{
+  let ancho = window.innerWidth
+
+    if(ancho < 600){
+      celular = true;
+    }
+    else{
+      celular = false;
+    }
+  }
+
+  window.addEventListener('load', definirancho);
+  window.addEventListener('load', definirancho);
+
   const HandleClick = () => {
    setcerrado(!cerrado);
   }
+
+
 
 return (
 <>
 
 
 <nav className="Nav flex h-full">
-  <ul className={` ${cerrado ? '' : 'translate-x-0'} bg-F-blue min-h-full select-none`} onMouseLeave={HandleClick}>
+  <ul className={` ${cerrado ? 'menu' : 'translate-x-0 menu'}bg-F-blue min-h-full select-none`} onMouseLeave={HandleClick}>
    <BotonCerrar cerrado={cerrado} HandleClick={HandleClick}/>
-      <li className="flex justify-center">Inicio</li>
-        <li className="flex justify-center">Institucional
+      <li className="justify-center">Inicio</li>
+        <li className="justify-center">Institucional
         
         <ul className="submenu">
+          <span><span className="linea"></span></span>
               <li>Reseña Histórica</li>
               <li>Misión</li>
               <li>Visión</li>
@@ -35,9 +53,10 @@ return (
       </li>
 
 
-      <li className="flex justify-center">Normativas
+      <li className="justify-center">Normativas
         
         <ul className="submenu">
+        <span><span className="linea"></span></span>
               <li>PEI</li>
               <li>Manual<br/>de<br/>convivencia</li>
               <li>SIE</li>
@@ -46,9 +65,10 @@ return (
         
       </li>
 
-      <li className="flex justify-center">Gobierno<br/>Escolar
+      <li className="justify-center">Gobierno<br/>Escolar
 
         <ul className="submenu">
+        <span><span className="linea"></span></span>
               <li>Consejo<br/>Directivo</li>
               <li>Consejo<br/>Académico</li>
               <li>Consejo<br/>Estudiantil</li>
@@ -57,9 +77,10 @@ return (
         
       </li>
       
-      <li className="flex justify-center centrado">Personal
+      <li className="justify-center centrado">Personal
       
         <ul className="submenu">
+        <span><span className="linea"></span></span>
               <li>Directivos<br/>Docente</li>
               <li>Docentes</li>
               <li>Personero</li>
@@ -70,20 +91,21 @@ return (
       
       </li>
 
-      <li className="flex justify-center">Áreas</li>
+      <li className="justify-center">Áreas</li>
 
-      <li className="flex justify-center">Inscripciones
+      <li className="justify-center">Inscripciones
         
         <ul className="submenu">
+        <span><span className="linea"></span></span>
           <li>Documentos<br/>requeridos</li>
           <li>Formulario<br/>inscripcion</li>
         </ul>
 
       </li>
 
-        <li className="flex justify-center">Contratación</li>
-        <li className="flex justify-center">Egresados</li>
-        <li className="flex justify-center">Galería</li>
+        <li className="justify-center">Contratación</li>
+        <li className="justify-center">Egresados</li>
+        <li className="justify-center">Galería</li>
   </ul>
  <BotonHamburguesa cerrado={cerrado} HandleClick={HandleClick}/>
 </nav>
