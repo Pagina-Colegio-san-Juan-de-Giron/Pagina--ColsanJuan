0b61d2d50ca28d4855d99acfa2c47a6761ed1935
Pagina-Colegio-san-Juan-de-Giron <paginacolsanjuandegiron@santander.edu.co>
2024-11-17 16:30:15 -0500

    google api obliterated and Empty Contratacion DB

--- app/Componentes/footer.tsx
+++ app/Componentes/footer.tsx
@@ -49,9 +49,6 @@ const footer = () => {
                      </div>
                 </div>
 
-                <div className='mapa-contenedor'>
-                  <Mapa/>
-                </div>
               </div>
               <div className='lema'>
               <h1 className={alice.className}>¡Creemos en la excelencia como el camino para lograr los sueños!</h1>
